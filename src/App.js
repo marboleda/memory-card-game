bb46d0dae2b612cc5fb7b49e5b7c7683fc4168c4
Marco Arboleda <arboleda.marco@gmail.com>
2020-09-20 19:50:24 -0400

    Set up initial state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
+  const [order, setOrder] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+  const [clicked, setClicked] = useState(Array(12).fill(false));
+
   return (
     <div className="App">
     </div>
